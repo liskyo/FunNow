58072fdb136f564e49eb5b236ca50084b626adfa
liskyo <liskyo5470@gmail.com>
2024-04-08 22:31:25 +0800

    add checkin dataGridView where

--- FunNow/BackSide_POS/FrmPOS.cs
+++ FunNow/BackSide_POS/FrmPOS.cs
@@ -107,7 +107,20 @@ namespace FunNow.BackSide_POS
 
             var hotels = from h in db.Hotel   // 所有的hotel
                          where rooms.ToList().Contains(h.HotelID) //r.HotelID = h.HotelID 找出尚有空房的旅館 List<Hotel>
-                         select h;                                 
+                         select
+                         new
+                         {
+                             城市 = h.City.CityName,
+                             旅館名稱 = h.HotelName,
+                             旅館地址 = h.HotelAddress,
+                             旅館特色 = h.HotelDescription,
+                             旅館電話 = h.HotelPhone,
+                             旅館種類 = h.HotelType.HotelTypeName,
+                             均價 = h.Room.Average(p => p.RoomPrice)
+
+                             //旅館評價 = cr.Description,
+
+                         };  // 將hotels查詢結果繫結到dataGridView1                                
 
 
             // 將查詢結果繫結到資料表
